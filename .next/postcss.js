34c7f363e85c608adc738e14268e0cfdacb850c6
AmeerDraz <ameerabudraz1@gmail.com>
2025-10-22 20:02:24 +0300

    Remove .next folder from Git tracking

--- .next/postcss.js
+++
@@ -1,6 +0,0 @@
-var R=require("./build/chunks/[turbopack]_runtime.js")("postcss.js")
-R.c("build/chunks/[turbopack-node]_transforms_postcss_ts_b76e36d6._.js")
-R.c("build/chunks/[root-of-the-server]__422f7717._.js")
-R.m("[turbopack-node]/globals.ts [postcss] (ecmascript)")
-R.m("[turbopack-node]/ipc/evaluate.ts/evaluate.js { INNER => \"[turbopack-node]/transforms/postcss.ts { CONFIG => \\\"[project]/Work-worood-weba/worood-weba/postcss.config.mjs [postcss] (ecmascript)\\\" } [postcss] (ecmascript)\", RUNTIME => \"[turbopack-node]/ipc/evaluate.ts [postcss] (ecmascript)\" } [postcss] (ecmascript)")
-module.exports=R.m("[turbopack-node]/ipc/evaluate.ts/evaluate.js { INNER => \"[turbopack-node]/transforms/postcss.ts { CONFIG => \\\"[project]/Work-worood-weba/worood-weba/postcss.config.mjs [postcss] (ecmascript)\\\" } [postcss] (ecmascript)\", RUNTIME => \"[turbopack-node]/ipc/evaluate.ts [postcss] (ecmascript)\" } [postcss] (ecmascript)").exports
--- .next/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"mz2H/0okPedHFCieUmCjyGPicWwrj6BDErT9/Gs45n0=\"\n}"
